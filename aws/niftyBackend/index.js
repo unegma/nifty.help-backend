db31b691b562969abc004c77e865211e5293d115
Timothy Coleman <timhc22@gmail.com>
2021-03-21 00:13:59 +0000

    Add rarible-sdk and returning items

--- aws/niftyBackend/index.js
+++ aws/niftyBackend/index.js
@@ -4,6 +4,7 @@ const {
   SLACK_ERROR_LOG, // NEED ENVIRONMENT VARIABLES FOR THOSE USED IN IMPORTS
 } = process.env;
 
+const { RaribleSDK } = require("rarible-sdk");
 const { SlackErrorLogger } = require('@unegma/logger');
 const slackErrorLogger = new SlackErrorLogger(SLACK_ERROR_LOG);
 
@@ -17,8 +18,10 @@ exports.handler = async (event, context) => {
   console.log(`# Beginning ${AWS_LAMBDA_FUNCTION_NAME}`); console.log(JSON.stringify(event)); console.log(context);
   let message = "# Success";
   try {
-    throw new Error();
+    const raribleSDK = new RaribleSDK('mainnet'); // THIS CURRENTLY ONLY WORKS WITH MAINNET
+    const raribleItems = await raribleSDK.getItems();
 
+    return raribleItems;
   } catch(error) {
     message = error.message;
     await slackErrorLogger.logError('handler', `${AWS_LAMBDA_FUNCTION_NAME} failed.`, error);
